e51c82d84c5c9816792e43b5db3cab5a3ab25f8f
nanuk007 <utkarsh.5698@gmail.com>
2020-02-29 10:19:21 +0530

    final update

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup } from '@angular/forms';
+import { DatabaseService } from '../services/database.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -9,14 +11,42 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterPage implements OnInit {
 
+  products: Observable<any[]>;
+  developers : any;
+  developer = {};
+  product = {};
+ 
+  email: string;
   form: FormGroup;
-  constructor(private  router:  Router) { }
+  constructor(private  router:  Router,private db: DatabaseService) { }
 
   ngOnInit() {
+    this.db.getDatabaseState().subscribe(rdy => {
+      if (rdy) {
+        this.db.getDevs().subscribe(devs => {
+          this.developers = devs;
+        })
+        this.products = this.db.getProducts();
+      }
+    });
   }
 
+
   register(form) {
-      this.router.navigateByUrl('/home');
+    
+    try{
+
       console.log("my values"+JSON.stringify(form.value));
+      this.db.addUserData(this.email,JSON.stringify(form.value))
+      if(this.db.loadUserData(this.email)){
+      alert("Register succesfull");
+      }
+      this.router.navigateByUrl('/home');
+      
+
+    }catch(e){
+      console.log(e)
+    }
+      
   }
 }
