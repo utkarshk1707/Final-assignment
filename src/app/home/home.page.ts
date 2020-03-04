f03d3cd03698f5698aa6afe7c4c337ba82407e10
nanuk007 <utkarsh.5698@gmail.com>
2020-03-05 00:08:53 +0530

    added firestore db for hotels

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular'
+import { Platform, LoadingController } from '@ionic/angular'
 import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation';
 import { element, error } from 'protractor';
-import {GoogleMaps} from '@ionic-native/google-maps'
+import { GoogleMaps } from '@ionic-native/google-maps'
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
-import { AngularFirestore} from 'angularfire2/firestore';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
- 
-var google = require('@ionic-native/google-maps') ;
+import { computeStackId } from '@ionic/angular/directives/navigation/stack-utils';
 
+var google = require('@ionic-native/google-maps');
 
 @Component({
   selector: 'app-home',
@@ -19,109 +19,128 @@ var google = require('@ionic-native/google-maps') ;
 })
 export class HomePage {
 
- 
-  
+
+  isLoading =true;
+  places = new Array();
+  data = true;
   options: GeolocationOptions;
   currentPos: Geoposition;
-  mapElement:HTMLElement;
-  places: Array<any>;
-  hotels:[
-    {  
-      "id":1,
-      "name":"Hotel-1"
-   },
-   {  
-      "id":2,
-      "name":"Hotel-2"
-   }
-  ]
-  constructor(public platform: Platform,  public afAuth: AngularFireAuth,private firestore: AngularFirestore,private router: Router) {
+  mapElement: HTMLElement;
 
-    this.getHotelsdata();
+  async present() {
+    this.isLoading = true;
+    return await this.loadingController.create({
+      spinner: "bubbles"
+    }).then(a => {
+      a.present().then(() => {
+        console.log('presented');
+        if (!this.isLoading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+        }
+      });
+    });
+  }
+
+  async dismiss() {
+    this.isLoading = false;
+    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
+  }
+  constructor(public platform: Platform, public afAuth: AngularFireAuth, private firestore: AngularFirestore, private router: Router,public loadingController:LoadingController) {
+
+    this.getHotelsDbData();
     this.platform.ready().then(() => {
 
-    Geolocation.getCurrentPosition().then((position) => {
+      Geolocation.getCurrentPosition().then((position) => {
 
-      console.log(position.coords.latitude, position.coords.longitude)
-      var gps = new google.map.LatLng(position.coords.latitude, position.coords.longitude)
-      var radius = 500;
-      var tHis =this;
-      console.log(gps)
-      this.getHotels(gps, radius).then((results:Array<any>)=>{
+        console.log(position.coords.latitude, position.coords.longitude)
+        var gps = new google.map.LatLng(position.coords.latitude, position.coords.longitude)
+        var radius = 500;
+        var tHis = this;
+        console.log(gps)
+        this.getHotels(gps, radius).then((results: Array<any>) => {
 
-        results.forEach((element)=>{
+          results.forEach((element) => {
 
-          tHis.places.push(element);
-        });
-        console.log(this.places);
-      },(status)=>console.log("status: "+status));
+            console.log(element);
+            // tHis.places.push(element);
+          });
+          // console.log(this.places);
+        }, (status) => console.log("status: " + status));
+      });
+
     });
 
-    });
-   
-     
+
   }
 
-  getHotelsdata(){
+  getHotelsDbData() {
+
+
+    // let setDoc = this.firestore.collection('hotels').doc('4').set({
+    //   name: 'hotel-4'
+    // });
+
+    // console.log(setDoc);
+    this.present()
+    try {
+
+      let hotelsREf = this.firestore.collection('hotels').get().subscribe(
+        (snapshot) => {
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data().name);
+            this.places.push(doc.data().name);
+
+          });
+        }
+      )
+      if(this.places.length > 1){
+        this.data =false;
+      }
+      console.log(this.places);
+      console.log(hotelsREf);
 
     
-    let setDoc = this.firestore.collection('hotels').doc('4').set({
-      name: 'hotel-4'
-    });
-    let hotelsREf = this.firestore.collection('hotels').get()
-
-    console.log(setDoc);
-
-    console.log(hotelsREf);
-  }
-
-//   addData(){
-
-//     this.hotels.forEach(function(obj) {
-//       firebase.firestore().collection("hotels").add({
-//           id: obj.id,
-//           name: obj.name
-//       }).then(function(docRef) {
-//           console.log("Document written with ID: ", docRef.id);
-//       })
-//       .catch(function(error) {
-//           console.error("Error adding document: ", error);
-//       });
-//   }
-// }
-
-async signOut(){
-  try {
-    await  this.afAuth.auth.signOut().then(function(res) {
-      // Sign-out successful.
-      console.log(res);
-      this.router.navigateByUrl('/login');
-
-    alert("Logout Successfull");
-
-    }).catch(function(error) {
-      // An error happened.
+      this.dismiss()
+    } catch (error) {
+      
+      this.data = true;
       console.log(error);
-    });
-
+    }
     
   }
-  catch (err) {
-    console.log(err);
-  }
 
-}
+  async signOut() {
+    try {
+      await this.afAuth.auth.signOut().then(function (res) {
+        // Sign-out successful.
+        console.log(res);
+        this.router.navigateByUrl('/login');
+
+        alert("Logout Successfull");
+
+      }).catch(function (error) {
+        // An error happened.
+        console.log(error);
+      });
+
+
+    }
+    catch (err) {
+      console.log(err);
+    }
+
+  }
   getHotels(gps: any, radius: any) {
-    
-    
-  
-    this.mapElement=document.getElementById('map');
+
+
+
+    this.mapElement = document.getElementById('map');
     console.log(this.mapElement);
     var service = new google.maps.places.PlacesService(this.mapElement);
     let request = {
       location: gps,
       radius: radius,
-      types: ["hotels"]   
+      types: ["hotels"]
     };
     service.nearbySearch(request, (results, status) => {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -133,7 +152,7 @@ async signOut(){
 
     });
     return new Promise((resolve, reject) => {
-      
+
     });
 
   }
